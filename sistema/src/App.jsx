ec937f57f399a55f677e0ecb122befc0273e7359
Rodolfo <rqm@outlook.es>
2022-02-17 18:14:24 -0600

    telwin y router

---
+++ sistema/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        CRM React
+      </h1>
+    </>
+  )
+}
+
+export default App
