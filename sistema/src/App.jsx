bfa78623eb94c1551c56b5935b86811a958c3ebd
Rodolfo <rqm@outlook.es>
2022-02-17 20:36:01 -0600

    estilos listos

--- sistema/src/App.jsx
+++ sistema/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from './layauot/Layout';
+import EditarNuevoCliente from './paginas/EditarNuevoCliente';
+import Inicio from './paginas/Inicio';
+import NuevoCliente from './paginas/NuevoCliente'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>
-        CRM React
+        <BrowserRouter>
+          <Routes>
+            <Route path='/clientes' element={<Layout />}>
+              <Route index element={<Inicio />} />
+              <Route path='nuevo' element={<NuevoCliente />} />
+              <Route path='editar/:id' element={<EditarNuevoCliente />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </h1>
     </>
   )
---
+++ sistema/src/paginas/EditarNuevoCliente.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditarNuevoCliente = () => {
+    return (
+        <div>
+            <h1>EditarNuevoCliente</h1>
+        </div>
+    );
+};
+
+export default EditarNuevoCliente;
